62cbfd9c3d0681a4907363f1d24821a04ab28781
Alban Lorillard <alorillard@oui.sncf>
2021-10-06 11:38:52 +0200

    test: refactor generatePoints

--- pi.test.js
+++ pi.test.js
@@ -6,7 +6,7 @@ function approxPi(pts) {
 }
 
 
-test('Should approximate pi with 100 000 random pts with a precision of one digit', () => {
+describe('Pi approximation test', () => {
 
     const generatePoints = (n) => {
         const pts = [];
@@ -18,37 +18,31 @@ test('Should approximate pi with 100 000 random pts with a precision of one digi
         return pts;
     }
 
-    // Arrange
-    const pts = generatePoints(100000);
+    test('Should approximate pi with 100 000 random pts with a precision of one digit', () => {
 
-    // Act
-    const result = approxPi(pts);
+        // Arrange
+        const pts = generatePoints(100000);
 
-    // Assert
-    console.log(result);
-    expect(result).toBeCloseTo(3.14, 1);
-})
+        // Act
+        const result = approxPi(pts);
 
-test('Should approximate pi with 1 000 000 random pts with a precision of two digit', () => {
+        // Assert
+        console.log(result);
+        expect(result).toBeCloseTo(3.14, 1);
+    })
 
-    const generatePoints = (n) => {
-        const pts = [];
-        for (let i = 0; i< n ; i++) {
-            const x = Math.random();
-            const y = Math.random();
-            pts.push([x,y]);
-        }
-        return pts;
-    }
+    test('Should approximate pi with 1 000 000 random pts with a precision of two digit', () => {
 
-    // Arrange
-    const pts = generatePoints(1000000);
+        // Arrange
+        const pts = generatePoints(1000000);
 
-    // Act
-    const result = approxPi(pts);
+        // Act
+        const result = approxPi(pts);
 
-    // Assert
-    console.log(result);
-    expect(result).toBeCloseTo(3.14, 2);
+        // Assert
+        console.log(result);
+        expect(result).toBeCloseTo(3.14, 2);
+
+    })
 
 })
